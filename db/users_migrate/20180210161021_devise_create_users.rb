ab395b80c6b79dbebd8f8094d64c93a4bb1780d7
leo-midli <charmandermaster07@gmail.com>
2021-08-28 12:01:22 -0400

    Added database configeration

--- db/users_migrate/20180210161021_devise_create_users.rb
+++ db/users_migrate/20180210161021_devise_create_users.rb
@@ -39,10 +39,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
     add_column :users, :username, :string
     add_index :users, :username, unique: true
 
-    User.all.each do |user|
-      user.update_attribute(:username, "name_#{user.id}")
-    end
-
     change_column_null :users, :username, false
     add_column :users, :role, :string
     add_index :users, :email,                unique: true
--- db/users_migrate/20200418220518_create_abouts.rb
+++
@@ -1,6 +0,0 @@
-class CreateAbouts < ActiveRecord::Migration[6.0]
-  def change
-    add_column :abouts, :linkgithub, :text
-    add_column :abouts, :githubtitle, :text
-  end
-end
