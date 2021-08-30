1363faa583fef623b677f9debf3928efba4efa16
leo-midli <charmandermaster07@gmail.com>
2021-08-29 20:24:21 -0400

    Added custom title names

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,8 +1,10 @@
 class AccountsController < ApplicationController
   def data
+    @page_title = "Data | LionNav"
   end
 
   def personal
     @user = current_user
+    @page_title = "Personal Info | LionNav"
   end
 end
